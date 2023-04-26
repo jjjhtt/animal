98f42b2904999864c488a80d31ab0be3a2958c88
季颢天 <20373256@buaa.edu.cn>
2023-04-26 20:52:54 +0800

    11

--- config/index.js
+++ config/index.js
@@ -1,12 +1,6 @@
 export const config = {
   /** 是否使用mock代替api返回 */
-<<<<<<< HEAD
   domain: 'https://anith2.2022martu1.cn',
-  //domain: 'http://pyyyf4.natappfree.cc',
-
-=======
-  //domain: 'https://anith2.2022martu1.cn',
-  domain: 'http://pyyyf4.natappfree.cc'
->>>>>>> master
+  //domain: 'http://yt4fm9.natappfree.cc',
 };
 
