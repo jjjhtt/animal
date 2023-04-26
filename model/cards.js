93d9e1cf8cdf728ad5d5f26461923c171de83967
季颢天 <20373256@buaa.edu.cn>
2023-04-26 10:22:14 +0800

    try
    try

--- model/cards.js
+++
@@ -1,7 +0,0 @@
-import { gentweet } from './card';
-
-export function gettweetsList(baseID = 0, length = 10) {
-  return new Array(length).fill(0).map((_, idx) => gentweet(idx + baseID));
-}
-
-export const tweetsList = gettweetsList();
