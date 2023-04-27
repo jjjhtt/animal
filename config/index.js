d149e05dc785b5be4718a8db5166376caa8ff685
LiShuaiChao <2606565321@qq.com>
2023-04-27 10:42:01 +0800

    过吧

--- config/index.js
+++ config/index.js
@@ -1,14 +1,5 @@
 export const config = {
   /** 是否使用mock代替api返回 */
   domain: 'https://anith2.2022martu1.cn',
-<<<<<<< HEAD
-<<<<<<< HEAD
-  //domain: 'http://r8byky.natappfree.cc'
-=======
-  //domain: 'http://pyyyf4.natappfree.cc'
->>>>>>> master
-=======
-  //domain: 'http://pyyyf4.natappfree.cc'
->>>>>>> master
 };
 
