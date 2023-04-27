1e8d7717e91dac6612ee2b91a66e86b9d5058e0c
LiShuaiChao <2606565321@qq.com>
2023-04-27 10:38:07 +0800

    op

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export const config = {
   /** 是否使用mock代替api返回 */
-  //domain: 'https://anith2.2022martu1.cn',
-  domain: 'http://r8byky.natappfree.cc'
+  domain: 'https://anith2.2022martu1.cn',
+  //domain: 'http://r8byky.natappfree.cc'
 };
 
--- pages/login/forgetpw/forgetpw.js
+++ pages/login/forgetpw/forgetpw.js
@@ -1,5 +1,6 @@
 // pages/login/forgetpw/forgetpw.js
 import {config} from '../../../config/index'
+import Toast from 'tdesign-miniprogram/toast/index'
 Page({
   data: {
     email:'',
@@ -63,12 +64,21 @@ Page({
     this.setData({ ma: e.detail.value })
   },
   requestma: function() {
+    var self = this
     console.log(this.data.email)
       wx.request({
-        url: config.domain + '/user/registerRequest',
+        url: config.domain + '/user/resetPasswordRequest',
         data: { email: this.data.email },
         method: 'POST',
         success: function(res) {
+          Toast({
+            context: this,
+            selector: '#t-toast',
+            message: res.data.message,
+          });
+          if (res.data.code != 0) {
+            self.setData({email:''})
+          }
           console.log(res)
         }
       })
@@ -76,4 +86,41 @@ Page({
   returnlogin: function() {
       wx.redirectTo({ url: '../login', })
   },
+  postregister: function() {
+    self = this
+    wx.request({
+      url: config.domain + '/user/resetPasswordVerify',
+      data: { 
+        email: this.data.email,
+        verification: this.data.ma },
+      method: 'POST',
+      success: function(res) {
+        console.log(res)
+        if (res.data.code == 0) {
+          Toast({
+            context: this,
+            selector: '#t-toast',
+            message: '重置成功',
+            theme: 'success',
+            direction: 'column',
+          });
+          setTimeout(() => {
+            wx.navigateBack()
+            }, 2000)
+        } else {
+          Toast({
+            context: this,
+            selector: '#t-toast',
+            message: res.data.message,
+            theme: 'error',
+            direction: 'column',
+          });
+        }
+        self.setData({
+          email: '',
+          ma: ''
+        })
+      }
+    })
+  }
 })
